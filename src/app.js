7b64fef158ff18cb5efcb673e8201854a1226d40
gazort-cyber <gazort125@gmail.com>
2025-04-24 02:11:31 +0800

    Create app.js

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import CustomRealtime from './components/CustomRealtime';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>实时套利工具</h1>
+      <CustomRealtime />
+    </div>
+  );
+}
+
+export default App;
