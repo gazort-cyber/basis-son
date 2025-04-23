47b38e735670e05f2224cc591980a97d3dc914df
gazort-cyber <gazort125@gmail.com>
2025-04-24 02:16:56 +0800

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,12 @@
+// src/index.js
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css'; // 可选：你的自定义样式
+import App from './App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
